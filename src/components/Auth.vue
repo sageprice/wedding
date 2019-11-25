d9ff4983938595463c98f7f725d36e79ffff1e98
sageeprice <sageeprice@gmail.com>
2019-11-24 21:53:42 -0500

    add password check

---
+++ src/components/Auth.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="auth">
+    <h2>Password pls</h2>
+    <div>Password pls <input v-model="password" /></div>
+    <img
+      id="believe"
+      src="../assets/i-want-to-believe.jpg"
+      title="stay woke"
+      width="20%"
+      v-bind:style="this.isWoke ? 'display:inline' : 'display:none'"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "auth",
+  data() {
+    return { password: "", isWoke: false };
+  },
+  watch: {
+    password(newPw) {
+      const pw = newPw.toLowerCase();
+      localStorage.pw = pw;
+      // lol client-side auth. pls don't hack me
+      if (pw === "epsteindidntkillhimself" || pw === "staywoke") {
+        this.password = "";
+        this.isWoke = true;
+      } else if (pw === "thepriceisright" || pw === "12345") {
+        this.$router.push("/");
+      } else if (pw === "ourunion") {
+        this.password = "";
+        window.open("https://www.youtube.com/watch?v=U06jlgpMtQs");
+      }
+    }
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 
 import App from "./components/App.vue";
+import Auth from "./components/Auth.vue";
 import Home from "./components/Home.vue";
 import Deets from "./components/Deets.vue";
 import Faq from "./components/Faq.vue";
@@ -16,12 +17,46 @@ const router = new VueRouter({
   mode: "history",
   base: __dirname,
   routes: [
-    { path: "/", component: Home, name: "home" },
-    { path: "/deets", component: Deets, name: "deets" },
-    { path: "/faq", component: Faq, name: "faq" },
-    { path: "/registry", component: Registry, name: "buy us stuff" },
-    { path: "/rsvp", component: Rsvp, name: "rsvp" }
+    { path: "/", component: Home, name: "home", meta: { requiresAuth: true } },
+    {
+      path: "/deets",
+      component: Deets,
+      name: "deets",
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/faq",
+      component: Faq,
+      name: "faq",
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/registry",
+      component: Registry,
+      name: "buy us stuff",
+      meta: { requiresAuth: true }
+    },
+    {
+      path: "/rsvp",
+      component: Rsvp,
+      name: "rsvp",
+      meta: { requiresAuth: true }
+    },
+    { path: "/auth", component: Auth, name: "auth" }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    // lol client-side auth. pls don't hack me
+    if (localStorage.pw != "12345" && localStorage.pw != "thepriceisright") {
+      next("/auth");
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 // Update page title based on route name.
