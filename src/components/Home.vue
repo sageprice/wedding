d9efc2250034ada951cc5732c5eed347a06904af
sageeprice <sageeprice@gmail.com>
2020-01-05 16:51:43 -0500

    Reordering home page text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,14 @@
 <template>
   <div id="home">
     <p>
-      If you're here, ideally you have been invited to our wedding. If you've
-      found this by some other means, please appreciate the majesty of Mollycat.
-    </p>
-    <img src="../assets/GRACE.jpg" title="fearful leader" width="40%" />
-    <p>
       We're getting married! Please join us on September 19, 2020 to celebrate!
     </p>
     <p>
-      Mollycat will not be officiating.
+      If you're here, ideally you have been invited to our wedding. If you've
+      found this by some other means, please appreciate the majesty of Mollycat,
+      who will not be officiating.
     </p>
+    <img src="../assets/GRACE.jpg" title="fearful leader" width="40%" />
   </div>
 </template>
 
